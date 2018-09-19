0f6c74e919b0fa8491bbb0fc8bf522a262c982f2
azreal45 <jgconelly@gmail.com>
2018-09-19 11:39:10 -0700

    Rosalind Python Problem 3

---
+++ stringsandlists.py
@@ -0,0 +1,8 @@
+#Strings and Lists
+
+def stringcutter(s, a, b, c, d):
+	new_string = s[a:(b+1)] + " " + s[c:(d+1)]
+	print(new_string)
+
+stringcutter("X6IDv2mD65v1bP6EutamiasJmsx9Ktpu9DlXl2zNUWKCF0dPmWLAgaisnJZPwQW9E4Kwik5O8LgfHjkBFI9souSULVMtM9DYrkmYy74ZjktBjxVTZv0JKq3LIIYpN4uaFRFXCuSFfQQnyLbAk5uT2K4RkiiwrEuJirHo8S4q0oFE06dcornixdkXLZFa4x9YAkGVig.",15, 22, 175, 180)
+
