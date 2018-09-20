c142cdd478f44af90932bfc1090b73c5dfe94c62
azreal45 <jgconelly@gmail.com>
2018-09-20 08:35:22 -0700

    Adding comment on algorithm used

--- mortalfibonaccirabbits.py
+++ mortalfibonaccirabbits.py
@@ -3,7 +3,7 @@
 def mortal_rabbits(n, m):
     rabbits = [1] + [0]*(m - 1)
     for i in range(n - 1):
-        rabbits = [sum(rabbits[1:])] + rabbits[:-1]
+        rabbits = [sum(rabbits[1:])] + rabbits[:-1] #Effectively adding the terms from (k−m) to (k−2) to get the population. 
     print(sum(rabbits))
 
 mortal_rabbits(98, 17)
