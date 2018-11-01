07516c9011a6c188c62df4a9493b65faf7c01d31
azreal45 <jgconelly@gmail.com>
2018-10-31 21:15:21 -0700

    Counting Point Mutations and Edits

--- removelinebreaks.py
+++ removelinebreaks.py
@@ -1,5 +1,5 @@
 def removeLineBreaks():
-	f = open("/Users/Azreal/Downloads/rosalind_gc2.txt", "r")
+	f = open("/Users/Azreal/Downloads/rosalind_gc3.txt", "r")
 	contents = f.read()
 	string_fixed = contents.replace("\n","")
 	print (string_fixed)
