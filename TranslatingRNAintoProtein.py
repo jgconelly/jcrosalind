5015249a33851128e8df145470b3d04f41758ac8
azreal45 <jgconelly@gmail.com>
2018-11-02 11:18:27 -0700

    Trying to translate into Dict

--- TranslatingRNAintoProtein.py
+++ TranslatingRNAintoProtein.py
@@ -1,3 +1,10 @@
 #Translating RNA into Protein
 
-def translateRNA(s):
+def removeLineBreaks():
+	f = open("/Users/Azreal/Desktop/RNACodonTable.rtf", "r")
+	contents = f.read()
+	string_fixed = contents.replace("	","")
+	print (string_fixed)
+removeLineBreaks()
+
+#def translateRNA(s):
