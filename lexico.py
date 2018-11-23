09994b462e3173036bf826fbc90454f349b0759e
azreal45 <jgconelly@gmail.com>
2018-11-23 10:01:48 -0800

    Solution for Enumerating K-mers

--- lexico.py
+++ lexico.py
@@ -1,16 +1,17 @@
 import itertools
 import numpy as np
+
 def lexico(string, number):
-	result = itertools.product(string, repeat=number)
+	clean_string = string.replace(" ", "")
+	result = itertools.product(clean_string, repeat=number)
 	list_result = list(result)
-	chars = "(,')"
 	empty = []
 	for item in list_result:
 		empty.append(str(item))
+	#although this looks gross, it does what I need it to. Getting rid of the extra characters from the list of lists is 
+	#required to make this answer work. 
+	for item in empty:
+		new_item = ((((item.replace("(", "")).replace(")", "")).replace(", ", "")).replace("'", ""))
+		print (new_item)
 
-	final_list = [item.replace(chars, "") for item in empty if chars in item]
-	for item in final_list:
-		print (item)
-	#print (list_result)
-
-lexico('ABCDEFGH', 3)	
+lexico('A B C D E F G H', 3)	
